17ae9922e26a71acab0d9ae11fdfe732984b8419
Henrykim1111 <henrykim9319@gmail.com>
2024-09-14 16:06:14 -0400

    Add project file

---
+++ CultureTap/CultureTapApp.swift
@@ -0,0 +1,17 @@
+//
+//  CultureTapApp.swift
+//  CultureTap
+//
+//  Created by Henry's Mac on 9/14/24.
+//
+
+import SwiftUI
+
+@main
+struct CultureTapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
