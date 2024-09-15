3404742635c962cba52fa632a76c48141bf25084
Harditya Sarvaiya <hardityathebest@gmail.com>
2024-09-15 06:47:16 -0400

    feat: resultview

---
+++ CultureTap/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingView.swift
+//  CultureTap
+//
+//  Created by Harditya Sarvaiya on 9/15/24.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
---
+++ CultureTap/QuizContext.swift
@@ -0,0 +1,18 @@
+//
+//  QuizContext.swift
+//  CultureTap
+//
+//  Created by Harditya Sarvaiya on 9/15/24.
+//
+
+import SwiftUI
+
+struct QuizContext: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuizContext()
+}
